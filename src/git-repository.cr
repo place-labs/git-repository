2aa61f732d93d8f1053e0f8564fc4eb56433df06
Stephen von Takach <steve@place.technology>
2022-05-14 03:14:07 +1000

    fix: missing branch param on base module

--- src/git-repository.cr
+++ src/git-repository.cr
@@ -5,7 +5,7 @@ module GitRepository
     VERSION = {{ `shards version "#{__DIR__}"`.chomp.stringify.downcase }}
   {% end %}
 
-  def self.new(repository : String | URI, username : String? = nil, password : String? = nil) : GitRepository::Interface
+  def self.new(repository : String | URI, username : String? = nil, password : String? = nil, branch : String? = nil) : GitRepository::Interface
     uri = repository.is_a?(URI) ? repository : URI.parse(repository)
     downcased_host = uri.host.try &.downcase
 
@@ -16,7 +16,7 @@ module GitRepository
     # case "www.github.com", "github.com"
     # once we add specific providers
     else
-      GitRepository::Generic.new(uri.to_s, username, password)
+      GitRepository::Generic.new(uri.to_s, username, password, branch.presence)
     end
   end
 end
