1eeb097d6612f7d670804e3f23a4aeeff097e4ac
Stephen von Takach <steve@place.technology>
2022-05-14 13:13:38 +1000

    fix(releases): add ability to specify how many releases to grab

--- src/git-repository/releases.cr
+++ src/git-repository/releases.cr
@@ -1,6 +1,6 @@
 require "./commit"
 
 module GitRepository::Releases
-  abstract def releases : Array(String)
+  abstract def releases(count : Int32 = 50) : Array(String)
   abstract def fetch_release(version : String, download_to_path : String | Path) : Nil
 end
